3a7faef473ae8bb46aa1c655308d6965e71eee55
odarf <70912176+odarf@users.noreply.github.com>
2021-06-29 12:06:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ token = 'NzYxMjA0Nzk2MTQ0MzUzMjgw.X3XNNQ.gfXvBTR2b6ko61YbV6aqGKLedec'
 bot = commands.Bot(command_prefix='!')
 bot.remove_command('help')
 
-reddit = praw.Reddit(client_id="zAmSKq4rw44kdA",
-                     client_secret="JYNBubsgry0rnrWJYBvev7m7zv8",
+reddit = praw.Reddit(client_id="",
+                     client_secret="",
                      user_agent="discordMemeBot",
                      redirect_uri="http://127.0.0.1:65010/authorize_callback")
 
